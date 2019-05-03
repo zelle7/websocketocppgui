d3e980cb48a325aedd0610d2c48102c605f39b82
Zelle <christian@zellot.at>
2019-05-03 22:46:10 +0200

    Add gui elements for websocket communication

--- run.py
+++ run.py
@@ -1,13 +1,47 @@
+import json
+from json import JSONDecodeError
+from threading import Thread
+from tkinter import messagebox
+from typing import Optional
+
+from urls import validate_url, URLParseException
+
 try:
     import tkinter as tk  # for python 3
 except:
     import Tkinter as tk  # for python 2
 
+import logging
+
 import pygubu
 import websocket
 
+logger = logging.getLogger('root')
+logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
+
+PREDEFINED_MESSAGE_MAP = {
+    'Heartbeat': '[2, "uuid", "Heartbeat", {}]',
+    'BootNotification': '[2, "uuid", "BootNotification", {"value":12}]',
+}
+
 
 class Application(pygubu.TkApplication):
+    ws_thread: Thread
+    wssurl: Optional[tk.Entry]
+    send_button: Optional[tk.Button]
+    connect_button: Optional[tk.Button]
+    connection_status: Optional[tk.Label]
+    data_to_send: Optional[tk.Text]
+    log_box: Optional[tk.Text]
+    predefined_messages: Optional[tk.Listbox]
+    button_predefined_message: Optional[tk.Button]
+    ws: Optional[websocket.WebSocketApp]
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.wssurl = None
+        self.ws = None
+
     def _create_ui(self):
         # 1: Create a builder
         self.builder = builder = pygubu.Builder()
@@ -17,19 +51,158 @@ class Application(pygubu.TkApplication):
 
         # 3: Create the widget using a master as parent
         self.mainwindow = builder.get_object('base_frame', self.master)
+        # fetch other elements we might need
+        self.wss_url = builder.get_object('wssurl')
+        self.send_button = builder.get_object('send_button')
+        self.connect_button = builder.get_object('connect_button')
+        self.connection_status = builder.get_object('connection_status')
+        self.predefined_messages = builder.get_object('predefined_messages')
+        self.button_predefined_message = builder.get_object('button_predefined_message')
+        self.data_to_send = builder.get_object('data_to_send', self.master)
+
+        self.log_box = builder.get_object('log_box', self.master)
+        self.log_box_scrollbar = builder.get_object('log_box_scrollbar', self.master)
+        self.log_box['yscrollcommand'] = self.log_box_scrollbar.set
+        self.log_box_scrollbar['command'] = self.log_box.yview
+
+        self.set_resizable()
+
+        self.builder.connect_callbacks(self)
+        logger.debug("_create_ui finished")
+
+    def _init_after(self):
+        """
+        Loads the predefined messages and puts it as a selection into the predefined messages box
+        :return:
+        """
+        for key in PREDEFINED_MESSAGE_MAP.keys():
+            self.predefined_messages.insert(tk.END, key)
 
     def on_connect_clicked(self):
-        self.ws = websocket.WebSocketApp("ws://echo.websocket.org/",
-                                    on_message=self.write_to_text_box,
-                                    on_error=self.write_to_text_box,
-                                    on_close=self.write_to_text_box)
+        """
+        Triggered when a user clicks the connect button. If there is no connection running we connect to
+        the websocket server and update the texts in ui to disconnect etc. If there is a connection
+        we will disconnect from the websocket server.
+
+        :return:
+        """
+        if self.ws:
+            self.reset_state_after_socket_close()
+            return
+
+        url = self.wss_url.get()
+        try:
+            validate_url(url)
+        except URLParseException as e:
+            messagebox.showinfo('Message', 'Please provide a valid url: {}'.format(e))
+            return
+
+        self.ws = websocket.WebSocketApp(url,
+                                         on_message=self.on_message_received,
+                                         on_error=self.on_error_socket,
+                                         on_close=self.on_close_socket)
+        self.run_ws()
+        logger.debug("on_connect_clicked")
+        self.master.focus()
+        self.connect_button.config(text="disconnect")
+        self.connection_status.config(text="connected")
+        self.send_button.config(state=tk.ACTIVE)
+
+    def run_ws(self):
+        """
+        Run a the websocket client in a separate thread, as we would block the main UI thread otherwise
+        :return: void
+        """
+        import threading
+        self.ws_thread = threading.Thread(target=self.ws.run_forever)
+        self.ws_thread.start()
+
+    def on_send_button_clicked(self):
+        """
+        Click handler for send button. Will gather the content of the data_to_send element and send it to
+
+        :return:
+        """
+        if not self.ws:
+            messagebox.showinfo('Message', 'Please connect to a websocket server first')
+            return
+        message = self.data_to_send.get(1.0, tk.END)
+        try:
+            json.loads(message)
+        except JSONDecodeError:
+            messagebox.showerror("Invalid json", "Please provide a valid json object for sending")
+            return
+        self.write_to_text_box('> {}'.format(message))
+        self.ws.send(data=message)
+
+    def on_use_predefined_message_clicked(self):
+        """
+        Triggered when a user clicks the button beneath predefined messages. Looks up the message
+        for the selection and paste it into the messages box, so that the user can still edit it.
+
+        :return:
+        """
+        selection = self.predefined_messages.curselection()
+        if not selection:
+            messagebox.showerror('Nothing selected', "Please select a predefined message first")
+        try:
+            new_message = PREDEFINED_MESSAGE_MAP[self.predefined_messages.get(selection)]
+        except KeyError:
+            messagebox.showerror('Error on pasting', 'Could not find data for selection')
+            return
+        self.data_to_send.delete("1.0", tk.END)
+        self.data_to_send.insert(tk.END, new_message)
 
     def write_to_text_box(self, message):
-        print(message)
+        """
+        Writes a string to the log box text area
+        :param message:
+        :return:
+        """
+        message_to_print = message
+        if not message.endswith('\n'):
+            message_to_print = '{}\n'.format(message)
+        self.log_box.insert(tk.END, message_to_print)
+
+    def on_close_socket(self):
+        """
+        Called if the websocket closes the connection
+        :return:
+        """
+        self.write_to_text_box('connection closed')
+
+    def on_error_socket(self, e):
+        """
+        Triggered if an error happens during the socket communication
+        :param e:
+        :return:
+        """
+        self.write_to_text_box('connection closed because of error {}'.format(e))
+
+    def on_message_received(self, message):
+        """
+        Writes an incoming socket message to the log box.
+        :param message:
+        :return:
+        """
+        self.write_to_text_box('< {}'.format(message))
+
+    def reset_state_after_socket_close(self):
+        """
+        Resets the state of the websocket and according gui elements after a connection close
+        :return:
+        """
+        self.ws.close()
+        self.connect_button.config(text="connect")
+        self.connection_status.config(text="not connected")
+        self.send_button.config(state=tk.DISABLED)
+        self.ws = None
+        self.ws_thread.join(1)
 
 
 websocket.enableTrace(True)
 if __name__ == '__main__':
     root = tk.Tk()
     app = Application(root)
+    logger.debug("start app now")
     root.mainloop()
